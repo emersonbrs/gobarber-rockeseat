27bf183c36f21c25234978897a3eba399527564e
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-01 14:19:44 -0300

    Marcando notificações como lidas

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -21,6 +21,16 @@ class NotificationController {
 
     return res.json(notifications);
   }
+
+  async update(req, res) {
+    const notification = await Notification.findByIdAndUpdate(
+      req.params.id, // O req.params.id vem da rota
+      { read: true },
+      { new: true }
+    );
+
+    return res.json(notification);
+  }
 }
 
 export default new NotificationController();
