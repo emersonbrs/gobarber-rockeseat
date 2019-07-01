e9f0fe762724c42ae35464a99337c8a866d3b8bc
Emerson Barbosa <emersonbarbosa.contato@gmail.com>
2019-07-01 18:24:12 -0300

    Criando variáveis ambiente usando .env(environment variables)

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: '2525',
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '47cdc14a0f876b',
-    pass: '4c696f3db3b1bd',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,4 @@
 export default {
-  dsn: 'https://2c5fc8c0790a4ef7b16a83191a23ea1b@sentry.io/1494590',
+  // dsn: 'https://2c5fc8c0790a4ef7b16a83191a23ea1b@sentry.io/1494590',
+  dsn: process.env.SENTRY_DSN,
 };
